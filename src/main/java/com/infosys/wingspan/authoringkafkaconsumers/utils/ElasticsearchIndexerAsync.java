01d0fe9730db842a5d5c4a1e80e4ccda801d2222
Hritik <hritikm46@gmail.com>
2020-08-27 12:39:09 +0530

    Array of StackTraceElement to String fix for SearchIndexerImpl errors logging

--- src/main/java/com/infosys/wingspan/authoringkafkaconsumers/utils/ElasticsearchIndexerAsync.java
+++ src/main/java/com/infosys/wingspan/authoringkafkaconsumers/utils/ElasticsearchIndexerAsync.java
@@ -2,18 +2,18 @@
                Use of this source code is governed by GPL v3 license that can be found in the LICENSE file or at https://opensource.org/licenses/GPL-3.0
                This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License version 3" */
 /*
-© 2017 - 2019 Infosys Limited, Bangalore, India. All Rights Reserved. 
+© 2017 - 2019 Infosys Limited, Bangalore, India. All Rights Reserved.
 Version: 1.10
 
 Except for any free or open source software components embedded in this Infosys proprietary software program (“Program”),
 this Program is protected by copyright laws, international treaties and other pending or existing intellectual property rights in India,
 the United States and other countries. Except as expressly permitted, any unauthorized reproduction, storage, transmission in any form or
-by any means (including without limitation electronic, mechanical, printing, photocopying, recording or otherwise), or any distribution of 
+by any means (including without limitation electronic, mechanical, printing, photocopying, recording or otherwise), or any distribution of
 this Program, or any portion of it, may result in severe civil and criminal penalties, and will be prosecuted to the maximum extent possible
 under the law.
 
 Highly Confidential
- 
+
 */
 package com.infosys.wingspan.authoringkafkaconsumers.utils;
 
@@ -27,6 +27,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -65,7 +66,7 @@ public class ElasticsearchIndexerAsync {
                 indexDoc.put("locale", locale);
                 indexDoc.put("node_type", nodeType);
                 indexDoc.put("exception_message", samzaError);
-                indexDoc.put("exception_stack_trace", stackTrace);
+                indexDoc.put("exception_stack_trace", Arrays.toString(stackTrace));
 
                 IndexRequest indexRequest = new IndexRequest();
                 indexRequest.index("search_error_index");
